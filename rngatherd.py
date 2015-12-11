129a225726c255da3cd309432b727c58838d9086
pinae <jonny@pinae.net>
2015-12-11 17:48:39 +0100

    Reading hwrng, writing to /dev/hwrandom

--- rngatherd.py
+++ rngatherd.py
@@ -1,2 +1,37 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+from multiprocessing import Process, Queue
+import os
+import time
+
+DEVICE = os.path.join(os.path.sep, "dev", "hwrandom")
+
+
+def read_hwrng(q):
+    while True:
+        if q.full():
+            time.sleep(0.1)
+        else:
+            with open(os.path.join(os.path.sep, "dev", "hwrng"), 'rb') as whrng:
+                q.put(whrng.read(8))
+
+
+def write_to_pipe(q):
+    pipe = os.open(DEVICE, os.O_WRONLY)
+    while True:
+        os.write(pipe, q.get())
+
+
+if __name__ == '__main__':
+    if not os.path.exists(DEVICE):
+        os.mkfifo(DEVICE)
+    q = Queue(1024)
+    gather_process = Process(target=read_hwrng, args=(q, ))
+    gather_process.daemon = True
+    gather_process.start()
+    while True:
+        try:
+            write_to_pipe(q)
+        except BrokenPipeError:
+            pass
