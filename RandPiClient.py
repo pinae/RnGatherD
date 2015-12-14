7ab0fb973790d35674b575573af3f5e47b392bb8
pinae <jonny@pinae.net>
2015-12-14 12:09:25 +0100

    Integrated RandPi client

---
+++ RandPiClient.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import hmac
+import sys
+from base64 import b64decode
+
+import requests
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256, SHA384, HMAC
+from pbkdf2 import PBKDF2
+
+SHARED_SECRET = ("Mein tolles langes Passwort, das total sicher ist. " +
+                 "Das sieht man an den Sonderzeichen wie / (Slash) oder $ (Dollar). " +
+                 "Außerdem enthält diese Passphrase einfach eine Menge Zeichen, " +
+                 "die ein Angreifer erst mal erraten muss.")
+SHARED_SALT = "pepper"
+base_key = PBKDF2(SHARED_SECRET.encode('utf-8'), SHARED_SALT.encode('utf-8'),
+                  iterations=32000, digestmodule=SHA384, macmodule=HMAC).read(48)
+ENCRYPTION_KEY = base_key[:32]
+ENCRYPTION_IV = base_key[32:48]
+
+
+def remove_pkcs7_padding(data):
+        return data[:-data[-1]]
+
+
+def get_random(length=64):
+    request = requests.get('http://127.0.0.1:8000/entropy/random?length=' + str(length))
+    data = request.json()
+    if hmac.new(ENCRYPTION_KEY,
+                b64decode(data['encrypted_data']),
+                SHA256).digest() != b64decode(data['hmac']):
+        print("Wrong signature!")
+        exit(1)
+    cipher = AES.new(ENCRYPTION_KEY, AES.MODE_CBC, ENCRYPTION_IV)
+    data = remove_pkcs7_padding(cipher.decrypt(b64decode(data['encrypted_data'])))
+    return data
+
+if __name__ == "__main__":
+    sys.stdout.buffer.write(get_random())
--- rngatherd.py
+++ rngatherd.py
@@ -4,8 +4,11 @@
 from multiprocessing import Process, Queue
 import os
 import time
+from RandPiClient import get_random
 
 DEVICE = os.path.join(os.path.sep, "dev", "hwrandom")
+READ_HWRNG = True
+READ_RAND_PI = False
 
 
 def read_hwrng(q):
@@ -15,6 +18,14 @@ def read_hwrng(q):
         else:
             with open(os.path.join(os.path.sep, "dev", "hwrng"), 'rb') as whrng:
                 q.put(whrng.read(8))
+
+
+def read_rand_pi(q):
+    while True:
+        if q.full():
+            time.sleep(1)
+        else:
+            q.put(get_random(1024))
 
 
 def write_to_pipe(q):
@@ -27,9 +38,14 @@ if __name__ == '__main__':
     if not os.path.exists(DEVICE):
         os.mkfifo(DEVICE)
     q = Queue(1024)
-    gather_process = Process(target=read_hwrng, args=(q, ))
-    gather_process.daemon = True
-    gather_process.start()
+    if READ_HWRNG:
+        gather_hwrng_process = Process(target=read_hwrng, args=(q,))
+        gather_hwrng_process.daemon = True
+        gather_hwrng_process.start()
+    if READ_RAND_PI:
+        gather_rand_pi_process = Process(target=read_rand_pi, args=(q,))
+        gather_rand_pi_process.daemon = True
+        gather_rand_pi_process.start()
     while True:
         try:
             write_to_pipe(q)
